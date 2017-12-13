27218b44199684e72a89f8959b4b6aa0fc99f3e3
kyle hamilton <kyleih@optonline.net>
2017-12-13 21:37:41 +0000

    paths

--- server/server.js
+++ server/server.js
@@ -48,14 +48,27 @@ if (isDeveloping) {
   app.use(webpackHotMiddleware(compiler));
 
   app.get('/', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist', 'index.html')));
     res.end();
   });
 } else {
   app.use(express.static(__dirname + '/dist'));
   app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
+
+  /*
+
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+});
+
+
+  */
+
+
+
 }
 app.use(SSE);
 
